e53cd4f32e78c1f0a4efb7449d459e48bd0f475f
hexitex <rmcglade@googlemail.com>
2018-01-29 13:47:16 +0000

    wrong code

--- tictaktoe.py
+++ tictaktoe.py
@@ -1,9 +1,9 @@
 import random
 import os
 import copy
+from _functools import reduce
 
 master_grid=[[4,4,4],[4,4,4],[4,4,4]]
-empty_grid=copy.deepcopy(master_grid[:])
 player1_score=0
 player2_score=0
 player1=''
@@ -12,33 +12,26 @@ valid_first=('a','b','c')
 valid_second=('1','2','3')
 abc_map={'a':0,'b':1,'c':2}
 exit_flag=0             
-#start by getting the grid5
 cp=-1   
 player=''
 os.system('cls')  # For Windows
 os.system('clear')  # For Linux/OS X            
 
 def init():
-    #empty_grid=list(copy.deepcopy(master_grid))
     global player1
     global player2
     global player1_score
     global player2_score
-    #global empty_grid
     player1_score=0
     player2_score=0
-    print ('/----------------------------------------------------------------------\\')
-    print ('| Welcome to the awesome  gameplay feature of this ASCII ticTacToe  |')
-    print ('| in the known universe. Targets are in the form of A1 or 1A, C2 or 2c. |')
-    print ('| "Q", "q" or "quit" to end game, and "R", "r" or "restart" to restart  |')
-    print ('\\----------------------------------------------------------------------/\n\n')
+    print ('/------------------------------------------------------------------------\\')
+    print ('| ASCII ticTacToe - "q" to end game, and "r" to restart                   |')
+    print ('| Targets are in the form of A1 or 1A, C2 or 2c. This allows bigger grids |')
+    print ('\\------------------------------------------------------------------------/\n\n')
     player1=input('Enter Player 1 Name: ')+' (P1)'
     if len(player1)==0: player1='Lazy Brain (P1)'
     player2=input('Enter Player 2 Name: ')+' (P2)'
     if len(player2)==0: player2='Lazy Git (P2)'
-    #cg=empty_grid
-   # show_grid(cg)
-    return empty_grid
 
 def show_grid(cg):
     out='\n\n\n\n{p1} Score: {p1s} {p2} Score: {p2s}\n     A     B     C\n'.format(p1s=player1_score, p2s=player2_score,p1=player1,p2=player2)
@@ -61,10 +54,8 @@ def show_grid(cg):
                 out+='  |\n'
     print (out+'  +-----+-----+-----+')
 
-    
 def check_target(target,cg):
-    #has restart or quit beeen requested?
-    if len(target)<2 and target.lower() not in['r','q']:
+    if len(target)<2 and target.lower() not in['r','q']:#has restart or quit been requested?
         return [True,-1,-1]
     if target.lower()[0]=='q' :
         return [True,-5,-5]
@@ -73,8 +64,7 @@ def check_target(target,cg):
     t1=target[0].lower()
     t2=target[1].lower()
     
-    #allow both '1a' and 'a1' to be valid as input
-    if (t1 in valid_first) and (t2 in valid_second):
+    if (t1 in valid_first) and (t2 in valid_second): #allow both '1a' and 'a1' to be valid as input
         v1=abc_map[t1]
         v2=int(t2)-1
     elif (t2 in valid_first) and (t1 in valid_second):
@@ -91,78 +81,42 @@ def check_target(target,cg):
     return [True,v2,v1]
 
 def is_finished(cg):
-    global player1_score
-    global player2_score
- 
+    
+    who_won = list(map(lambda x,y,z: x+y+z,cg[0],cg[1],cg[2])) #vertical wins
+    a=check_line(who_won)
+    if a[0]==1:
+        return a
+
+    new_list=list(zip(cg[0],cg[1],cg[2])) # horizontal wins
+    who_won = list(map(lambda x,y,z: x+y+z,new_list[0],new_list[1],new_list[2]))
+    a=check_line(who_won)
+    if a[0]==1:
+        return a
+    
+    a=check_line([cg[0][0]+cg[1][1]+cg[2][2],cg[1][1]+cg[0][2]+cg[2][0],4]) # diagonal wins
+    if a[0]==1:
+        return a
    
-    for x in cg:
-        p1won=0
-        p2won=0
-        for i in x:
-            if i==0:
-                p1won+=1
-            elif i==1:
-                p2won+=1
-        if p1won==3:
-            player1_score+=1
-            return [1,'Player 1 Wins Horiz',1]
-        if p2won==3:
-            player2_score+=1
-            return [1,'Player 2 Wins Horiz',1]
-   
-    for x in range(0,3):
-        a1=0
-        a2=0
-        a3=0
-        a1+=cg[0][x]
-        a2+=cg[1][x]
-        a3+=cg[2][x]
-
-        #print (str(x)+str(a1)+str(a2)+str(a3))
-
-        if a1+a2+a3==0:
-            player1_score+=1
-            return [1,'Player 1 Wins Vert',1]
-        if a1+a2+a3==3:
-            player2_score+=1
-            return [1,'Player 2 Wins Vert',1]
-    a1=0
-    a2=0
-    a3=0
-    a4=0
-    a5=0
-   
-    a1=cg[0][0]
-    a2=cg[1][1]
-    a3=cg[2][2]
-    a4=cg[0][2]
-    a5=cg[2][0]
-
-    if a1+a2+a3==0:
-            player1_score+=1
-            return [1,'Player 1 Wins Diag LR',1]
-    if a1+a2+a3==3:
-            player2_score+=1
-            return [1,'Player 2 Wins Diag LR',1]
-
-    if a2+a4+a5==3:
-            player2_score+=1
-            return [1,'Player 2 Wins Diag RL',1]
-    if a2+a4+a5==0:
-            player1_score+=1
-            return [1,'Player 1 Wins Diag RL',1]
-
-    pos_count=0
-
+    pos_count=0 # full grid
     for x in cg:
         for a in x:
             if a==1 or a==0:
                 pos_count+=1
-        
     if pos_count>=9:
         return [1,'Full Board',1]
       
     return [0,'Game in Progress',0]
+
+def check_line(line):
+    global player1_score
+    global player2_score
+    if line[0]==0 or line[1]==0 or line[2]==0:
+        player1_score+=1
+        return [1,'Player 1 Wins ',1]
+    if line[0]==3 or line[1]==3 or line[2]==3:
+        player2_score+=1
+        return [1,'Player 2 Wins ',1]
+    return[0,'',0]
         
 def take_turn(cp):
     global player1
@@ -178,8 +132,7 @@ def take_turn(cp):
 def run_game(cg):
     global exit_flag
     
-    while not exit_flag:
-        # start of game
+    while not exit_flag:  # start of game
         if random.randrange(0,2)==0:
             cp=0
             player=player1
@@ -195,7 +148,6 @@ def run_game(cg):
             show_grid(cg)
             run=check_target(take_turn(cp),cg)
             if not run[0]:
-                # running
                 cg[run[1]][run[2]]=cp #make move
                 fin=is_finished(cg)
 
@@ -209,29 +161,23 @@ def run_game(cg):
                 cp= 0 if cp else 1 # flip player
      
             else:
-                
-                #invalid?
-                if run[1]==-1 or run[1]==-3:
-                    #no target try again 
-                    print('\n\n\n\n\n'+player+' Try again!')
+              
+                if run[1]==-1 or run[1]==-3:  #invalid?
+                    print('\n\n\n\n\n'+player+' Try again!')#no target try again 
                     run[0]=0
-                if run[1]==-2:
-                    #restart wanted
+                if run[1]==-2:#restart wanted
                     cg=copy.deepcopy(master_grid)
                     break
-                if run[1]==-5:
-                    #quit wanted
+                if run[1]==-5: #quit wanted
                     if player1_score>player2_score:
-                       player=player1
-                       print (player+' WINS!')
+                        player=player1  
+                        print (player+' WINS!')
                     elif player2_score>player1_score:
-                       player=player2
-                       print (player+' WINS!')
+                        player=player2
+                        print (player+' WINS!')
                     else:
-                       print ('DRAW!')
-                   
+                        print ('DRAW!')
                     exit_flag=1
                     break
-           
 init()
-run_game(empty_grid) 
+run_game(copy.deepcopy(master_grid[:])) 
